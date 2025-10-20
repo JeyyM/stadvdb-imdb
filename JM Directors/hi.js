eb9388c1832617c6ac4d9f79c95c045b1ee661b6
JeyM <jeymson8000@gmail.com>
2025-10-20 17:04:53 +0800

    Added a worse query

--- JM Directors/hi.js
+++ JM Directors/hi.js
@@ -1,5 +1,13 @@
-ALTER TABLE directors_dt
-  ADD INDEX idx_validcount_avgrating(validCount, avgWeightedRating);
-  
-ALTER TABLE directors_dt
-  DROP INDEX idx_validcount_avgrating
+SELECT
+d.primaryName,
+  COUNT(*) AS totalWorks,
+    SUM((
+      SELECT(t2.startYear IS NULL OR t2.weightedRating IS NULL)
+        FROM title_ft t2
+        WHERE t2.tconst = t.tconst
+    )) AS missingInfoCount
+FROM directors_dt AS d
+JOIN title_director_bridge AS tdb ON d.nconst = tdb.nconst
+JOIN title_ft AS t ON t.tconst = tdb.tconst
+GROUP BY d.nconst, d.primaryName
+HAVING d.nconst = 'nm2078274';
